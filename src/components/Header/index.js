2521e47b22a9e568622bf7007450bc25f61bd757
Gevorg <gevgalbash@mail.ru>
2022-03-31 17:31:08 +0400

    layout component done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,43 @@
+import { AppBar,   Container, Toolbar, Typography, } from "@material-ui/core";
 import React from "react";
-import "./Header.css";
+import { makeStyles } from "@material-ui/styles"
+import { Paper, InputBase, IconButton, Button} from '@mui/material';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Header = () => {
+const useStyles = makeStyles({
+  root:{
+    justifyContent: "space-between",
+  }
+});
+export default function Header() {
+  const classes = useStyles()
+   return (
+     <AppBar position="static" color="primary">
+       <Container maxWidth="md">
+         <Toolbar className={classes.root}>
+           <Typography variant="h6" color="inherit">
+             Material UI Demo
+           </Typography>
+           <Paper
+              component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search Google Maps"
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+              <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+             <Toolbar>
+            <Button variant="contained">Sign up</Button>
+            <Button variant="contained">Sign in</Button>
+            </Toolbar>
+         </Toolbar>
+       </Container>
+     </AppBar>
+   );
+ }
 
-    return (
-
-      <nav className="Nav">
-
-        <div className="Nav-menus">
-
-          <div className="Nav-brand">
-
-            <a className="Nav-brand-logo" href="/">
-            </a>
-            <a className="instagramStyle">Instagram</a>
-          </div>
-
-        </div>
-
-      </nav>
-
-    );
-
-}
-
-export default Header;
