936360210a021499713bcf88bb14bb9ed081128b
dianna1234 <01234dianna@gmail.com>
2022-04-03 23:50:22 +0400

    changed

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -1,22 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import Register from '../pages/Register/Register';
-import { AuthProvider } from '../AuthProvider/AuthProvider';
-
 
 function Layout() {
 
     return (
-      <AuthProvider>
-        <div >
-          <Routes>
-  
-            <Route path='/login' element={<Login/>} />
-            <Route path='/register' element={<Register />}/>
-        
-          </Routes>
-        </div>
-      </AuthProvider>
+      <div></div>
     )
   }
   
