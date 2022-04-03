b5fcf13816167aea58118b325ce18905fa9ed825
dianna1234 <01234dianna@gmail.com>
2022-04-03 23:21:00 +0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Layout from './Layouts/Layout';
+// import Layout from './Layouts/Layout';
 import './App.css';
+import MainRoutes from './Routes/Routes';
 
 function App() {
   // return <div className="App" />;
-  return <Layout />
+  // return <Layout />
+  return <MainRoutes/>
 }
 
 export default App;
