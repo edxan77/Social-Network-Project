609674137a084477d39d4027b2133356061e1305
edxan77 <edgar.khanamiryan@gmail.com>
2022-04-03 13:25:12 +0400

    eslint new config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import './App.css';
-import MainFriends from './Components/MainUserFriends/MainUserFriendList';
-import Friendlist from './Components/FriendList/Friendlist';
 
 function App() {
-  return (
-    <div>
-      <MainFriends />
-      <Friendlist />
-    </div>
-  );
+  return;
 }
 
 export default App;
