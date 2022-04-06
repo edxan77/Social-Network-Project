7ecaab988d561b8896982e93d819bdb2715556d2
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-04-07 01:06:20 +0400

    changes for merge

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { Link } from 'react-router-dom';
-import { auth, db } from '../../lib/firebase';
+import { auth } from '../../lib/firebase';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { ref, set } from "firebase/database";
+// import { ref, set } from "firebase/database";
+import { addUser } from '../../Service/firestore';
 import { useFormik } from 'formik';
 import { useState, useEffect } from 'react';
 import styles from './Register.module.css';
-import { addUser } from '../../Service/firestore';
+
 
 
 function Register() {
@@ -77,7 +77,7 @@ function Register() {
     },[email, password])
   
   
-   function createAccount(){
+    function createAccount(){
       
       createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -89,16 +89,15 @@ function Register() {
         //   email:email,
         //   password:password
         // })
-
-        addUser({ 
-            id: user.uid,
-            firstName: firstName,
-            lastName:lastName,
-            displayName: user.displayName,
-            email:email,
-            password:password
-        })
-        
+        addUser({
+          id: user.uid,
+          firstName: firstName,
+          lastName:lastName,
+          email:email,
+          password:password,
+          follows:[],
+          followers:[]
+      })
         .then(()=>{
           setIsSubmeted(true);
           console.log('created')
@@ -147,7 +146,8 @@ function Register() {
                 <h4>It is quick and easy</h4>
               </div>
               <form onSubmit={formik.handleSubmit}>
-                <input type="text" className={styles.input}  placeholder='FirstName' id='firstName' name='firstName' 
+                <input type="text" className={formik.touched.firstName && formik.errors.firstName ? styles.inputError : styles.input}  
+                  placeholder='FirstName' id='firstName' name='firstName' 
                   onChange={formik.handleChange} 
                   onBlur={formik.handleBlur} 
                   value={formik.values.firstName} 
@@ -156,7 +156,8 @@ function Register() {
                   <div className={styles.error}>{formik.errors.firstName}</div> }
                 </div>
   
-                <input type="text" className={styles.input}  placeholder='LastName' id='lastName' name='lastName' 
+                <input type="text" className={formik.touched.lastName && formik.errors.lastName ? styles.inputError : styles.input} 
+                  placeholder='LastName' id='lastName' name='lastName' 
                   onChange={formik.handleChange}  
                   onBlur={formik.handleBlur} 
                   value={formik.values.lastName}  
@@ -165,22 +166,25 @@ function Register() {
                   <div className={styles.error}>{formik.errors.lastName}</div> }
                 </div>
   
-                <input type='email' className={styles.input} placeholder='Email' id="email"  name="email"
+                <input type='email' className={fireEmailError || formik.touched.email && formik.errors.email ? styles.inputError : styles.input} 
+                  placeholder='Email' id="email"  name="email"
                   onChange={formik.handleChange}  onBlur={formik.handleBlur}
                   value={formik.values.email}  
                 />
-                <div className={styles.err}>{formik.touched.email && formik.errors.email ?  <div className={styles.error}>{formik.errors.email}</div> :
-                  <div className={styles.error}>{fireEmailError}</div> }
+                <div className={styles.err}>{formik.touched.email && formik.errors.email ?  <div className={styles.error}>
+                  {formik.errors.email}
+                  </div> : <div className={styles.error}>{fireEmailError}</div> }
                 </div>
              
-              <input type="password"  className={styles.input} placeholder='Password' id="password" name="password"
-                onChange={formik.handleChange} 
-                onBlur={formik.handleBlur}
-                value={formik.values.password} 
-              />
-              <div className={styles.err}>{formik.touched.password && formik.errors.password ? <div className={styles.error}>{formik.errors.password}</div> : 
-                <div className={styles.error}>{firePasswordError}</div> }
-              </div>
+                <input type="password"  className={firePasswordError || formik.touched.password && formik.errors.password ? styles.inputError : styles.input} 
+                  placeholder='Password' id="password" name="password"
+                  onChange={formik.handleChange} 
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password} 
+                />
+                <div className={styles.err}>{formik.touched.password && formik.errors.password ? <div className={styles.error}>
+                  {formik.errors.password}</div> : <div className={styles.error}>{firePasswordError}</div> } 
+                </div>
    
                <div className={styles.line}></div>
                <button type='submit' className={styles.btn}>Sign up</button>
