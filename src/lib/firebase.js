0a6e339ad45d542eea8ed086c11bf5614c801923
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-04-09 10:37:08 +0400

    firebase link disabled

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,13 +14,13 @@ import { getFirestore } from 'firebase/firestore';
 // const appId = process.env.REACT_APP_APP_ID;
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDaTcGszQUQBGvzehp7dspQpYvk6On07L4',
-  authDomain: 'sosial-network-e2964.firebaseapp.com',
-  databaseURL: 'https://sosial-network-e2964-default-rtdb.firebaseio.com',
-  projectId: 'sosial-network-e2964',
-  storageBucket: 'sosial-network-e2964.appspot.com',
-  messagingSenderId: '267114802245',
-  appId: '1:267114802245:web:187c89edc13f60feb3c72c',
+  // apiKey: 'AIzaSyDaTcGszQUQBGvzehp7dspQpYvk6On07L4',
+  // authDomain: 'sosial-network-e2964.firebaseapp.com',
+  // databaseURL: 'https://sosial-network-e2964-default-rtdb.firebaseio.com',
+  // projectId: 'sosial-network-e2964',
+  // storageBucket: 'sosial-network-e2964.appspot.com',
+  // messagingSenderId: '267114802245',
+  // appId: '1:267114802245:web:187c89edc13f60feb3c72c',
 };
 
 // Initialize Firebase
