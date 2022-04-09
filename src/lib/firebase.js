26e36109def99323742b7eaf6bc33bd4ec9b8a00
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-04-09 10:36:24 +0400

    firestore links changed

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {getDatabase} from 'firebase/database';
 import { getAuth } from "firebase/auth";
 // added for firestore
@@ -16,13 +17,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDaTcGszQUQBGvzehp7dspQpYvk6On07L4',
-authDomain: 'sosial-network-e2964.firebaseapp.com',
-databaseURL: 'https://sosial-network-e2964-default-rtdb.firebaseio.com',
-projectId: 'sosial-network-e2964',
-storageBucket: 'sosial-network-e2964.appspot.com',
-messagingSenderId: '267114802245',
-appId: '1:267114802245:web:187c89edc13f60feb3c72c',
+  apiKey: "AIzaSyAUzr2-h7XDjxgI3OMql6vJpYuEz2zR0Pw",
+  authDomain: "social-network-ef434.firebaseapp.com",
+  projectId: "social-network-ef434",
+  storageBucket: "social-network-ef434.appspot.com",
+  messagingSenderId: "300929157083",
+  appId: "1:300929157083:web:0704c1fd05bed2916789fe",
+  measurementId: "G-FJDEF26910"
 }
   
 
@@ -34,5 +35,7 @@ export const db = getDatabase(app);
 
 // added for firestore
 export const firebase = getFirestore(app)
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
 
 export default app;
