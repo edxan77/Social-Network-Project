3e5d23694a7364af742117d9b040e1692682f512
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-04-15 01:30:32 +0400

    BG Image container style changed

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,21 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import { Avatar, Box, Button, CardMedia, Container, TextField } from '@mui/material';
+import { Box, Button, CardMedia, Container } from '@mui/material';
 import AccountMenu from '../../components/AccountMenu/AccountMenu';
 import AddNewPostForm from '../../components/Posts/postsComponents/AddNewPostForm';
 import PostsContent from '../../components/Posts/postsComponents/PostsContent';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import { getAllUsersById } from '../../Service/firestore';
-import {UrlContext} from '../../UrlProvider/UrlProvider'
-import {useEffect,useContext, useState} from 'react';
+import { UrlContext } from '../../UrlProvider/UrlProvider';
+import { useEffect, useContext, useState } from 'react';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+// import { Box } from '@mui/system';
 
 function UserProfile() {
-
   const [img, setImg] = useState();
-  const {currentUser} = useContext(AuthContext);
-  const {handleSubmit, handleImageChange, setLoading, loading, url, key, deleteImg} = useContext(UrlContext);
+  const { currentUser } = useContext(AuthContext);
+  const {
+    handleSubmit,
+    handleImageChange,
+    setLoading,
+    loading,
+    url,
+    key,
+    deleteImg,
+  } = useContext(UrlContext);
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -29,131 +37,137 @@ function UserProfile() {
   useEffect(() => {
     if (currentUser) {
       getAllUsersById(currentUser.uid).then((userData) => {
-        setImg(userData.map(data => data.backgroundImg));
+        setImg(userData.map((data) => data.backgroundImg));
         // console.log(userData);
       });
     }
   }, [currentUser, img, !img]);
 
-    useEffect(()=>{
-      setLoading(false);
-    },[url])
-
- 
+  useEffect(() => {
+    setLoading(false);
+  }, [url]);
 
   return (
     <Container
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        // width: '700',
-        maxWidth: 'md',
+        maxWidth: 'lg',
         alignItems: 'center',
-        // justifyContent: 'center',
       }}
     >
-      {/* <Container  sx={{
-        display: 'flex',
-        // flexDirection: 'column',
-        // width: '700',
-        maxWidth: 'md',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}> */}
-      {loading ? <div  style ={{
-          boxShadow:'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;',
-          borderBottomLeftRadius:'12px',
-          borderBottomRightRadius:'12px',
-          width:'1000px',
-          height:'380px',
-          display:'flex',
-          justifyContent:'center',
-          alignItems:'center'
-        }} 
-        
-        src={img ? `${img}` : ''}>{loading && <img src='http://www.avalonparis.com/wp-content/uploads/2017/01/loading5.gif'/>}</div> : 
-        <CardMedia component="img" height="380" sx={{
-            boxShadow:'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;',
-            borderBottomLeftRadius:'12px',
-            borderBottomRightRadius:'12px',
-            width:'1000px' 
-          }} 
-          src={img ? `${img}` : ''}
-        /> }
-        
-
-        <div>
-          {/* { img ?
-          <Button onClick={handleClick} 
-            style={{position:'absolute', marginLeft: '350px', marginBottom:'200px', color:'#000', backgroundColor:'fff'}}
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-          <h4 style={{color:'#000', textTransform:'capitalize'}}>Edit photo</h4>
-          <PhotoCameraIcon fontSize='medium' />
-          </Button> :
-           <Button onClick={handleClick} 
-           style={{position:'absolute', marginLeft: '350px', marginBottom:'200px', color:'#000', backgroundColor:'fff'}}
-           id="basic-button"
-           aria-controls={open ? 'basic-menu' : undefined}
-           aria-haspopup="true"
-           aria-expanded={open ? 'true' : undefined}
-           >
-           <h4 style={{color:'#000', textTransform:'capitalize'}}>Add photo</h4>
-           <PhotoCameraIcon fontSize='medium' />
-           </Button>
-        } */}
-            <Button onClick={handleClick} 
-            style={{position:'absolute', marginLeft: '350px', marginBottom:'200px', color:'#000', backgroundColor:'fff'}}
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-          <h4 style={{color:'#000', textTransform:'capitalize'}}>{img ? 'Edit photo' : 'Add photo'}</h4>
-          <PhotoCameraIcon fontSize='medium' />
-          </Button> 
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: 'lg',
+          width: '100%',
+          backgroundColor: '#5d70fc',
         }}
-        >
-        <MenuItem >
-          <form onSubmit={handleSubmit}>
-          <input type='file' onChange={handleImageChange}  />
-          <Button onClick={handleSubmit} style={{textTransform:'capitalize'}}>Upload</Button>
-          </form>
-        </MenuItem>
-        <MenuItem style={{  
-          display:'flex',
-          justifyContent:'center',
-          alignItems:'center'}}
-        > 
-          <Button onClick={()=> deleteImg(key)} style={{textTransform:'capitalize'}}>delete image</Button>
-        </MenuItem> 
-      </Menu>
-    </div>
-        
-        <AccountMenu />
-      {/* </Container> */}
+      >
+        {loading ? (
+          <div
+            style={{
+              boxShadow:
+                'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;',
+              borderBottomLeftRadius: '12px',
+              borderBottomRightRadius: '12px',
+              height: '23vw',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            src={img ? `${img}` : ''}
+          >
+            {loading && (
+              <img src="http://www.avalonparis.com/wp-content/uploads/2017/01/loading5.gif" />
+            )}
+          </div>
+        ) : (
+          <CardMedia
+            component="img"
+            sx={{
+              height: '25vw',
+              boxShadow:
+                'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;',
+              borderBottomLeftRadius: '12px',
+              borderBottomRightRadius: '12px',
+            }}
+            src={img ? `${img}` : ''}
+          />
+        )}
 
+        <div
+          style={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Button
+            onClick={handleClick}
+            style={{
+              position: 'absolute',
+              color: '#000',
+              backgroundColor: '#d4e3fa',
+              filter: 'drop-shadow(0px 3px 5px rgba(0,0,0,0.32))',
+            }}
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <h4 style={{ color: '#000', textTransform: 'capitalize' }}>
+              {img ? 'Edit photo' : 'Add photo'}
+            </h4>
+            <PhotoCameraIcon fontSize="medium" color="primary" />
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem>
+              <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleImageChange} />
+                <Button
+                  onClick={handleSubmit}
+                  style={{ textTransform: 'capitalize' }}
+                >
+                  Upload
+                </Button>
+              </form>
+            </MenuItem>
+            <MenuItem
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button
+                onClick={() => deleteImg(key)}
+                style={{ textTransform: 'capitalize' }}
+              >
+                delete image
+              </Button>
+            </MenuItem>
+          </Menu>
+        </div>
+      </Box>
+
+      <AccountMenu />
       <Container
         sx={{
           display: 'flex',
           flexDirection: 'column',
           width: '700',
-          // maxWidth: 'sm',
-          // alignSelf: 'center',
-          // justifySelf: 'center',
         }}
       >
-        {/* <UserPosts/> */}
         <AddNewPostForm />
         <PostsContent />
       </Container>
