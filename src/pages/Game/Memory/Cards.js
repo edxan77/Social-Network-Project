a1151f03c75ff83cfa4cf17bce9f0cba039cd607
Tarhovh <96591762+Tarhovh@users.noreply.github.com>
2022-04-15 14:37:25 +0400

    anused imports deleted

--- src/pages/Game/Memory/Cards.js
+++ src/pages/Game/Memory/Cards.js
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import React from 'react'
-import Card from './Card'
-import './Car.css'
+import { useState } from 'react';
+import Card from './Card';
+import './Car.css';
 
 function Cards(){
 const [items, setItems] = useState([
