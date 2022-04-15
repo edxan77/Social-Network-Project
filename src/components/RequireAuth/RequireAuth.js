fcde285cb9927fd32357c5c7dc64438c077d3384
edxan77 <edgar.khanamiryan@gmail.com>
2022-04-16 02:21:40 +0400

    reConsole

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,6 +1,6 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
-import { useContext } from 'react';
+import { useContext,useEffect } from 'react';
 import { RedirectContext } from '../../RedirectProvider/RedirectProvider';
 
 export default function RequireAuth({ children }) {
@@ -8,7 +8,7 @@ export default function RequireAuth({ children }) {
   const {isAllow} = useContext(RedirectContext)
   const location = useLocation();
 
-  if (currentUser === null && isAllow === true) {
+  if (currentUser === null && isAllow == true) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
